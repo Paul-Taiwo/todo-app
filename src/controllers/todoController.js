897edca77d2047cc54a3e14c1ffdea4817628c8d
Paul Taiwo <pauladedokun2000@gmail.com>
2019-01-17 14:04:15 +0100

    Add Database

--- src/controllers/todoController.js
+++ src/controllers/todoController.js
@@ -1,5 +1,42 @@
+import bodyParser from 'body-parser';
+import mongoose from 'mongoose';
+
+// Connect to database
+mongoose.connect('mongodb://test:PAULot2000.@ds121495.mlab.com:21495/users-todo');
+
+// Create a blueprint
+const todoSchema = new mongoose.Schema({
+  item: String,
+});
+// Create a model
+const Todo = mongoose.model('Todo', todoSchema);
+
+const urlencoded = bodyParser.urlencoded({ extended: false });
+
+// const data = [
+//   { item: 'get milk' },
+//   { item: 'kick some coding ass' },
+//   { item: 'play football' },
+// ];
+
 export default (app) => {
   app.get('/todo', (req, res) => {
-    res.render('todo');
+    Todo.find({}, (err, data) => {
+      if (err) throw err;
+      res.render('todo', { todos: data });
+    });
+  });
+
+  app.post('/todo', urlencoded, (req, res) => {
+    Todo(req.body).save((err, data) => {
+      if (err) throw err;
+      res.json(data);
+    });
+  });
+
+  app.delete('/todo/:item', (req, res) => {
+    Todo.find({ item: req.params.item.replace(/\-/g, ' ') }).deleteOne((err, data) => {
+      res.json(data);
+    });
   });
 };
--- src/public/assets/js/script.js
+++ src/public/assets/js/script.js
@@ -1,36 +1,31 @@
+let item;
 $(document).ready(() => {
+  $('form').on('submit', () => {
+    item = $('form input');
+    const todo = { item: item.val() };
 
-  $('form').on('submit', function(){
+    $.ajax({
+      type: 'POST',
+      url: '/todo',
+      data: todo,
+      success: (data) => {
+        // do something with the data via front-end framework
+        location.reload();
+      },
+    });
 
-      var item = $('form input');
-      var todo = {item: item.val()};
-
-      $.ajax({
-        type: 'POST',
-        url: '/todo',
-        data: todo,
-        success: function(data){
-          //do something with the data via front-end framework
-          location.reload();
-        }
-      });
-
-      return false;
-
+    return false;
   });
 
-  $('li').on('click', function(){
-      var item = $(this).text().replace(/ /g, "-");
-      $.ajax({
-        type: 'DELETE',
-        url: '/todo/' + item,
-        success: function(data){
-          //do something with the data via front-end framework
-          location.reload();
-        }
-      });
+  $('li').on('click', function () {
+    item = $(this).text().replace(/ /g, '-');
+    $.ajax({
+      type: 'DELETE',
+      url: `/todo/${item}`,
+      success: (data) => {
+        // do something with the data via front-end framework
+        location.reload();
+      },
+    });
   });
-
-} }
-
 });
