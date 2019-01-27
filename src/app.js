be9ce1c867c768b9b6d53ee712a3d01bf2f60312
Paul Taiwo <pauladedokun2000@gmail.com>
2019-01-27 15:23:46 +0100

    Fixed path issues

--- src/app.js
+++ src/app.js
@@ -1,5 +1,7 @@
 import express from 'express';
 import bodyParser from 'body-parser';
+import log from 'fancy-log';
+import path from 'path';
 
 import todoController from './controllers/todoController';
 
@@ -7,22 +9,29 @@ const app = express();
 
 const PORT = process.env.PORT || 4040;
 
-app.set('views', `${__dirname}\\views`);
+const viewsFolder = path.format({
+  dir: 'C:\\Users\\Paul Taiwo\\Documents\\web files\\todo-app\\src\\views',
+
+});
+
+log.info();
+
+app.set('views', viewsFolder);
 app.set('view engine', 'ejs');
 
-app.use(express.static(`${__dirname}\\public`));
+app.use(express.static(path.format({
+  dir: 'C:\\Users\\Paul Taiwo\\Documents\\web files\\todo-app\\src\\public',
+})));
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
 todoController(app);
 
-console.log(`${__dirname}\\views`);
-
 app.get('/', (req, res) => {
   res.send('Welcome');
 });
 
-app.listen(PORT, () => { console.log('Yo, Listening ', PORT); });
+app.listen(PORT, () => { log.info('Yo, Listening ', PORT); });
 
 export default app;
--- src/controllers/todoController.js
+++ src/controllers/todoController.js
@@ -1,8 +1,7 @@
-import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 
 // Connect to database
-mongoose.connect('mongodb://test:PAULot2000.@ds121495.mlab.com:21495/users-todo');
+// mongoose.connect('mongodb://test:PAULot2000.@ds121495.mlab.com:21495/users-todo');
 
 // Create a blueprint
 const todoSchema = new mongoose.Schema({
@@ -11,23 +10,21 @@ const todoSchema = new mongoose.Schema({
 // Create a model
 const Todo = mongoose.model('Todo', todoSchema);
 
-const urlencoded = bodyParser.urlencoded({ extended: false });
-
-// const data = [
-//   { item: 'get milk' },
-//   { item: 'kick some coding ass' },
-//   { item: 'play football' },
-// ];
+const data = [
+  { item: 'get milk' },
+  { item: 'kick some coding ass' },
+  { item: 'play football' },
+];
 
 export default (app) => {
   app.get('/todo', (req, res) => {
-    Todo.find({}, (err, data) => {
-      if (err) throw err;
-      res.render('todo', { todos: data });
-    });
+    // Todo.find({}, (err, data) => {
+    //   if (err) throw err;
+    // });
+    res.render('todo', { todos: data });
   });
 
-  app.post('/todo', urlencoded, (req, res) => {
+  app.post('/todo', (req, res) => {
     Todo(req.body).save((err, data) => {
       if (err) throw err;
       res.json(data);
